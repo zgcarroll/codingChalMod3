5269db656cfd7521b73652e32f8ffa3407e67523
zgcarroll <qc0193ki@go.minnstate.edu>
2025-09-17 21:08:32 +0000

    first commit

---
+++ Module3Challenge/Program.cs
@@ -0,0 +1,17 @@
+﻿// module 3 challenge
+
+// code from assignment description
+using System;
+
+namespace ZooAnimalsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the animal's hunger level (1-10): ");
+            int hungerLevel = int.Parse(Console.ReadLine());
+            //Add code here for your if-else statement.
+        }
+    }
+}
